636b0d024bfd42f110070cf2173e13bdf970d837
Cidmar da Silva Ribeiro <cidmardsr@gmail.com>
2018-05-24 10:13:05 -0300

    continuação

--- src/Custos.java
+++ src/Custos.java
@@ -14,15 +14,19 @@ public class Custos {
     double[] gastosComTelefoneEInternets = new double[12];
     int atual = 0;
     
-    public void menuDosCustos(){
-        int menuCustos = Integer.parseInt(
-            JOptionPane.showInputDialog(
-                "1- Cadastrar custos" +
-                "\n2- Editar custos" +
-                "\n3- Listar custos" +
-                "\n4- Buscar pelo mês" +
-                "\n5- Finalizar"));
-        while (menuCustos != 5) {
+    public class menuDosCustos{
+        private Custos custos = new Custos();
+        private int menuGastos(){
+        int menuCustos = 
+            JOptionPane.showOptionDialog(null, "MENU DOS CUSTOS", "CustosHotel", 0, 0, 
+            null, new Object[]{
+                "Cadastrar", "Editar", "Listar", "buscarPelo Mes", "Sair"
+            }, "Cadastrar");
+            return menuCustos;
+    }
+    public void organizarMenu(){
+        int menuCustos = menuGastos();
+        while (menuCustos != 4) {
             switch (menuCustos) {
                 case 1:
                     cadastrar();
@@ -36,18 +40,10 @@ public class Custos {
                 case 4:
                     buscarPeloMes();
                     break;
-                
-                default:
-                    JOptionPane.showMessageDialog(null, "Opção inválida");
             }
-            menuCustos = Integer.parseInt(JOptionPane.showInputDialog(
-                "1- Cadastrar custos" +
-                "\n2- Editar custos" +
-                "\n3- Listar custos" +
-                "\n4- Buscar pelo mês" +
-                "\n5- Finalizar"));
+            menuCustos = menuGastos();
         }
-        
+    
     }
 
     public void cadastrar() {
@@ -64,43 +60,43 @@ public class Custos {
         
         aguaCustos[mes-1] = Double.parseDouble(
             JOptionPane.showInputDialog(
-                "Digite o número equivalente ao mês que deseja editar." 
+                "Digite o número equivalente ao mês que deseja editar os gastos com Água." 
                 + "\nExemplo: Se deseja editar o mês de Outubro digite 10"
         ));
         
         energiaCustos[mes-1] = Double.parseDouble(
             JOptionPane.showInputDialog(
-                "Digite o número equivalente ao mês que deseja editar." 
+                "Digite o número equivalente ao mês que deseja editar os gastos com Energia." 
                 + "\nExemplo: Se deseja editar o mês de Outubro digite 10"
         ));
         
         gastosComMantimentos[mes-1] = Double.parseDouble(
             JOptionPane.showInputDialog(
-                "Digite o número equivalente ao mês que deseja editar." 
+                "Digite o número equivalente ao mês que deseja editar os gastos com Mantimentos." 
                 + "\nExemplo: Se deseja editar o mês de Outubro digite 10"
         ));
         
         gastosComManutencoes[mes-1] = Double.parseDouble(
             JOptionPane.showInputDialog(
-                "Digite o número equivalente ao mês que deseja editar." 
+                "Digite o número equivalente ao mês que deseja editar os gastos com Manutenção." 
                 + "\nExemplo: Se deseja editar o mês de Outubro digite 10"
         ));
         
         gastosComFuncionarios[mes-1] = Double.parseDouble(
             JOptionPane.showInputDialog(
-                "Digite o número equivalente ao mês que deseja editar." 
+                "Digite o número equivalente ao mês que deseja editar os gastos com Funcionários." 
                 + "\nExemplo: Se deseja editar o mês de Outubro digite 10"
         ));
         
         gastosComLimpezas[mes-1] = Double.parseDouble(
             JOptionPane.showInputDialog(
-                "Digite o número equivalente ao mês que deseja editar." 
+                "Digite o número equivalente ao mês que deseja editar os gastos com Limpeza." 
                 + "\nExemplo: Se deseja editar o mês de Outubro digite 10"
         ));
         
         gastosComTelefoneEInternets[mes-1] = Double.parseDouble(
             JOptionPane.showInputDialog(
-                "Digite o número equivalente ao mês que deseja editar." 
+                "Digite o número equivalente ao mês que deseja editar os gastos com Telefone e Internet." 
                 + "\nExemplo: Se deseja editar o mês de Outubro digite 10"
         ));
             
@@ -127,12 +123,8 @@ public class Custos {
                 + "\nExemplo: Se desejar visualizar o mês de Fevereiro digite 2"
         ));
         
-        for(int i = 0; i < atual; i++){
-           if(aguaCustos.equals(mesDesejado)){
-               apresentarInformacao(i);
-           }
+               apresentarInformacao(mesDesejado-1);
                
-           }
         }
     
     public void apresentarInformacao(int posicao){
@@ -150,33 +142,54 @@ public class Custos {
     public void solicitarInformacao(int posicao){
         aguaCustos[posicao] = Double.parseDouble(
             JOptionPane.showInputDialog(
-                "Informe o valor gasto com água"
-        ));
+                "Informe o valor gasto com água", 
+                    aguaCustos[posicao] != 0 ? aguaCustos[posicao] : 0)
+                .replace(" ", "").replace("RS", "")
+                .replace(".", "").replace(",", ".")
+        );
         energiaCustos[posicao] = Double.parseDouble(
             JOptionPane.showInputDialog(
-                "Informe o valor gasto com energia"
-        ));
+                "Informe o valor gasto com energia",
+                     energiaCustos[posicao] != 0 ? energiaCustos[posicao] : 0)
+                .replace(" ", "").replace("RS", "")
+                .replace(".", "").replace(",", ".")
+        );
         gastosComMantimentos[posicao] = Double.parseDouble(
             JOptionPane.showInputDialog(
-                "Informe o valor gasto com mantimentos"
-        ));
+                "Informe o valor gasto com mantimentos",
+                     gastosComMantimentos[posicao] != 0 ? gastosComMantimentos[posicao] : 0)
+                .replace(" ", "").replace("RS", "")
+                .replace(".", "").replace(",", ".")
+        );
         gastosComManutencoes[posicao] = Double.parseDouble(
             JOptionPane.showInputDialog(
-                "Informe o valor gasto com manutenção"
-        ));
+                "Informe o valor gasto com manutenção",
+                     gastosComManutencoes[posicao] != 0 ? gastosComManutencoes[posicao] : 0)
+                .replace(" ", "").replace("RS", "")
+                .replace(".", "").replace(",", ".")
+        );
         gastosComFuncionarios[posicao] = Double.parseDouble(
             JOptionPane.showInputDialog(
-                "Informe o valor gasto com fucionários"
-        ));
+                "Informe o valor gasto com fucionários",
+                     gastosComFuncionarios[posicao] != 0 ? gastosComFuncionarios[posicao] : 0)
+                .replace(" ", "").replace("RS", "")
+                .replace(".", "").replace(",", ".")
+        );
         gastosComLimpezas[posicao] = Double.parseDouble(
             JOptionPane.showInputDialog(
-                "Informe o valor gasto com limpeza"
-        ));
+                "Informe o valor gasto com limpeza",
+                     gastosComLimpezas[posicao] != 0 ? gastosComLimpezas[posicao] : 0)
+                .replace(" ", "").replace("RS", "")
+                .replace(".", "").replace(",", ".")
+        );
         gastosComTelefoneEInternets[posicao] = Double.parseDouble(
             JOptionPane.showInputDialog(
-                "Informe o valor gasto com telefone e internet"
-        ));
+                "Informe o valor gasto com telefone e internet",
+                     gastosComTelefoneEInternets[posicao] != 0 ? gastosComTelefoneEInternets[posicao] : 0)
+                .replace(" ", "").replace("RS", "")
+                .replace(".", "").replace(",", ".")
+        );
         
-
+        }
     }
 }
