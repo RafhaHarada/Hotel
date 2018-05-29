a53632b85694741e90c832c7f39570e1cc7f9c42
Rafael Harada <RafhaHarada@gmail.com>
2018-05-29 11:00:41 -0300

    oi

--- src/Principal.java
+++ src/Principal.java
@@ -1,3 +1,6 @@
+
+import javax.swing.JOptionPane;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -14,6 +17,11 @@ public class Principal {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Object[] itens = {"1","2","3"};
+        JOptionPane.showOptionDialog(null, "1- coisaz\n2- outro\n3- Bka", "asd", 0, JOptionPane.INFORMATION_MESSAGE, null, itens, itens[0]);
+        Object[] itens2 = {"dadasdads","2","3"};
+        JOptionPane.showOptionDialog(null, "1- coisaz\n2- outro\n3- Bka", "asd", 0, JOptionPane.INFORMATION_MESSAGE, null, itens2, itens2[0]);
+    
         Sistema sistema = new Sistema();
         sistema.apresentarMenu();
     }
